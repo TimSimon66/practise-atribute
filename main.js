7b046a5530f7491dbf40ac2946495b4d650f5cae
TimSimon66 <huynhvantin125ttt@gmail.com>
2023-07-12 19:18:51 +0700

    update

---
+++ main.js
@@ -0,0 +1,18 @@
+function changImage(id) {
+    let imga = document.getElementById(id).getAttribute('src');
+    document.getElementById('main').setAttribute('src', imga)
+}
+
+/*
+function changImage(id) {
+
+    let random = document.getElementById(id).getAttribute('src');
+
+    document.getElementById('main').setAttribute('src', random);
+}
+*/
+
+
+
+
+
